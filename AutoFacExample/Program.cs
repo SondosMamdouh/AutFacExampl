6ff4521b32e44b944328257885e7114b0c3508aa
SondosMamdouh <sondosmamdouh.sm@gmail.com>
2020-10-07 14:14:26 +0200

    first commit

---
+++ AutoFacExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using System;
+
+namespace AutoFacExample
+{
+    class Program
+    {
+          public interface IWeapon { void Kill(); }
+    public class Bazuka : IWeapon {
+        public void Kill() { Console.Write("BIG bum"); }
+    }
+
+    public class Sword : IWeapon {
+        public void Kill() { Console.Write("Chik-chik"); }
+    }
+    public class Warrior {
+        private readonly IWeapon _weapon;
+        public Warrior(IWeapon weapon) { _weapon = weapon; }
+        public void Kill() { _weapon.Kill(); }
+    }
+
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Bazuka>();
+            builder.RegisterType<Warrior>();
+            builder.RegisterType<Sword>();
+            builder.Register<IWeapon>(x => x.Resolve<Bazuka>());
+
+            var container = builder.Build();
+            var warrior = container.Resolve<Warrior>();
+            warrior.Kill();
+        }
+    }
+}
